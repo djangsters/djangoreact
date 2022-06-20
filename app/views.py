98836c27406a8028130fb97bcaa5628b0f48cf64
Michael Musangeya <phoexer@gmail.com>
2022-06-20 13:12:31 +0200

    Add template and base

--- app/views.py
+++ app/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    template = loader.get_template('app/index.html')
+    context = {
+        "Hello" : "Hello World - App"
+    }
+    return HttpResponse(template.render(context, request))
